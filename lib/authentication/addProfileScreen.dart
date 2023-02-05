cc685ed50845a25523962ee4369a57e74d833d86
HNx1 <107187909+HNx1@users.noreply.github.com>
2023-02-05 18:19:04 +0000

    Update addProfileScreen.dart

--- lib/authentication/addProfileScreen.dart
+++ lib/authentication/addProfileScreen.dart
@@ -27,7 +27,7 @@ class AddProfileScreen extends StatefulWidget {
 }
 
 class _AddProfileScreenState extends State<AddProfileScreen> {
-  final CurrenyYear = DateFormat('yyyy').format(DateTime.now());
+  final CurrentYear = DateFormat('yyyy').format(DateTime.now());
   String selectBirthDate = "";
   var formKey = GlobalKey<FormState>();
   TextEditingController _firstNameController = TextEditingController();
@@ -90,16 +90,26 @@ class _AddProfileScreenState extends State<AddProfileScreen> {
   @override
   Widget build(BuildContext context) {
     final objProviderNotifier = context.watch<ProviderNotifier>();
-    final objAuthrovider = context.watch<AuthenicationNotifier>();
+    final objAuthProvider = context.watch<AuthenicationNotifier>();
     return Scaffold(
       //--------------------Arrow back Icon------------------------------
       appBar: AppBar(
         backgroundColor: AppTheme.getTheme().scaffoldBackgroundColor,
-        leading: Container(),
+        leading: GestureDetector(
+          onTap: () {
+            Navigator.pop(context);
+          },
+          child: Icon(
+            Icons.arrow_back_ios_rounded,
+            color: ConstColor.primaryColor,
+            size: width * 0.06,
+          ),
+        ),
+        leadingWidth: height * 0.034,
         title:
             //--------------------Profile ------------------------------
             Text(
-          TextUtils.Profile,
+          "Fill out your Profile",
           style: AppTheme.getTheme().textTheme.bodyText2!.copyWith(
               color: ConstColor.primaryColor,
               height: 1.4,
@@ -110,7 +120,7 @@ class _AddProfileScreenState extends State<AddProfileScreen> {
       body: Stack(
         alignment: Alignment.center,
         children: [
-          objAuthrovider.isLoading == true
+          objAuthProvider.isLoading == true
               ? CircularProgressIndicator(
                   color: primaryColor,
                 )
@@ -150,179 +160,6 @@ class _AddProfileScreenState extends State<AddProfileScreen> {
 
                               ///---------------------------phone number textfrom field------------------
 
-                              Container(
-                                margin: EdgeInsets.only(top: height * 0.026),
-                                height: height * 0.077,
-                                decoration: BoxDecoration(
-                                  borderRadius:
-                                      BorderRadius.circular(height * 0.1),
-                                  border: Border.all(
-                                    color: ConstColor.textFormFieldColor,
-                                  ),
-                                ),
-                                child: Row(
-                                  children: [
-                                    ///--------------- Country Code---------------------
-                                    CountryListPick(
-                                      appBar: AppBar(
-                                        backgroundColor: Colors.black,
-                                        title: Text('Select Country '),
-                                      ),
-                                      theme: CountryTheme(
-                                        isShowFlag: true,
-                                        isShowTitle: false,
-                                        isShowCode: true,
-                                        isDownIcon: true,
-                                        showEnglishName: true,
-                                      ),
-                                      pickerBuilder: (context, code) {
-                                        return Row(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.center,
-                                          crossAxisAlignment:
-                                              CrossAxisAlignment.center,
-                                          children: [
-                                            Container(
-                                              margin: EdgeInsets.only(
-                                                  left: height * 0.01),
-                                              child: ClipRRect(
-                                                borderRadius:
-                                                    BorderRadius.circular(
-                                                        height * 0.1),
-                                                child: Image.asset(
-                                                  code!.flagUri!,
-                                                  fit: BoxFit.fill,
-                                                  width: width * 0.06,
-                                                  height: width * 0.06,
-                                                  package: 'country_list_pick',
-                                                ),
-                                              ),
-                                            ),
-                                            Container(
-                                              margin: EdgeInsets.only(
-                                                  left: width * 0.02,
-                                                  right: width * 0.01),
-                                              child: Text(
-                                                code.dialCode!,
-                                                style: AppTheme.getTheme()
-                                                    .textTheme
-                                                    .bodyText1!
-                                                    .copyWith(
-                                                      fontSize: width * 0.041,
-                                                      color: ConstColor
-                                                          .white_Color,
-                                                    ),
-                                              ),
-                                            ),
-                                            Icon(
-                                              Icons.keyboard_arrow_down_sharp,
-                                              color: ConstColor.white_Color,
-                                            ),
-                                            //------- divider-------------------
-                                            Container(
-                                              margin: EdgeInsets.only(
-                                                  right: height * 0.015,
-                                                  left: height * 0.01),
-                                              height: height * 0.023,
-                                              width: 1,
-                                              color: ConstColor.white_Color,
-                                            ),
-                                          ],
-                                        );
-                                      },
-                                      initialSelection: 'GB',
-                                      onChanged: (CountryCode? code) {
-                                        setState(() {
-                                          pickedCountryCode = code;
-                                        });
-                                      },
-                                      useUiOverlay: false,
-                                      useSafeArea: true,
-                                    ),
-
-                                    ///--------------------- Enter phone Textfield----------------
-                                    Expanded(
-                                      child: TextFormField(
-                                        textAlign: TextAlign.start,
-                                        inputFormatters: <TextInputFormatter>[
-                                          FilteringTextInputFormatter.digitsOnly
-                                        ],
-                                        controller: _PhoneController,
-                                        keyboardType: TextInputType.number,
-                                        style: AppTheme.getTheme()
-                                            .textTheme
-                                            .bodyText1!
-                                            .copyWith(
-                                              fontSize: width * 0.041,
-                                              color: ConstColor.white_Color,
-                                            ),
-                                        decoration: InputDecoration(
-                                          hintText: TextUtils.EnterphoneNum,
-                                          hintStyle: AppTheme.getTheme()
-                                              .textTheme
-                                              .bodyText1!
-                                              .copyWith(
-                                                fontSize: width * 0.041,
-                                                color: ConstColor.white_Color,
-                                              ),
-                                          errorStyle: AppTheme.getTheme()
-                                              .textTheme
-                                              .bodyText1!
-                                              .copyWith(
-                                                fontSize: width * 0.036,
-                                                fontWeight: FontWeight.w500,
-                                                color: Colors.red,
-                                              ),
-                                          border: InputBorder.none,
-                                        ),
-                                        onChanged: (val) {
-                                          setState(() {
-                                            phone = val;
-                                          });
-                                        },
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-
-                              ///------------- validation Text--------------
-                              phone == "" && isValidatePhone == true
-                                  ? Container(
-                                      margin: EdgeInsets.only(
-                                          top: height * 0.011,
-                                          left: width * 0.055),
-                                      child: Text(
-                                        TextUtils.entermobile,
-                                        style: AppTheme.getTheme()
-                                            .textTheme
-                                            .bodyText1!
-                                            .copyWith(
-                                              fontSize: width * 0.036,
-                                              fontWeight: FontWeight.w500,
-                                              color: Colors.red,
-                                            ),
-                                      ),
-                                    )
-                                  : phone.length != 10 &&
-                                          isValidatePhone == true
-                                      ? Container(
-                                          margin: EdgeInsets.only(
-                                              top: height * 0.011,
-                                              left: width * 0.055),
-                                          child: Text(
-                                            TextUtils.validmobile,
-                                            style: Theme.of(context)
-                                                .textTheme
-                                                .bodyText1!
-                                                .copyWith(
-                                                  fontSize: width * 0.036,
-                                                  color: Colors.red,
-                                                ),
-                                          ),
-                                        )
-                                      : Container(),
-
                               ///-----------------Birthday ---------------
                               GestureDetector(
                                 onTap: () async {
@@ -334,7 +171,7 @@ class _AddProfileScreenState extends State<AddProfileScreen> {
                                           initialDate: DateTime(1994),
                                           firstDate: DateTime(1960),
                                           lastDate: DateTime(
-                                            int.parse(CurrenyYear),
+                                            int.parse(CurrentYear),
                                           ),
                                           dateFormat: "dd-MMMM-yyyy",
                                           pickerMode: DateTimePickerMode.date,
@@ -573,32 +410,17 @@ class _AddProfileScreenState extends State<AddProfileScreen> {
                     setState(() {
                       isValidatePhone = true;
                     });
-                    var code =
-                        pickedCountryCode == "" || pickedCountryCode == null
-                            ? "+44"
-                            : pickedCountryCode.toString();
-                    String phoneNo =
-                        code + " " + "${_PhoneController.text.toString()}";
+
                     final isValid = formKey.currentState!.validate();
 
-                    if (isValid &&
-                        imageUrl != "" &&
-                        selectBirthDate != "" &&
-                        phone.length == 10 &&
-                        phone != "") {
-                      objProviderNotifier.setPhone = phoneNo;
-
-                      objAuthrovider.registeration(
+                    if (isValid && imageUrl != "" && selectBirthDate != "") {
+                      objAuthProvider.createProfile(
                         context: context,
                         firstName: _firstNameController.text,
-                        lastname: _LastNameController.text,
-                        emailId: objProviderNotifier.getemail,
-                        phoneNumber: phoneNo,
+                        lastName: _LastNameController.text,
                         birthdate: selectBirthDate,
                         imageUrl: imageUrl,
                         imageFile: imageFile!,
-                        role: "user",
-                        fcmToken: token!,
                       );
                     }
                   },
